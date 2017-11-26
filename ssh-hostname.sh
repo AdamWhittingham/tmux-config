449c1c08bea78c399574896325959193d45a3a1e
Adam Whittingham <adam.whittingham@gmail.com>
2017-11-26 15:25:32 +0000

    Add mosh support to hostname

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -2,9 +2,12 @@
 
 current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
 ssh_host=$(ps -t "$current_tty" | sed -n -e 's/^.*\(ssh\) //p')
+mosh_host=$(ps -t "$current_tty" | sed -n -e 's/^.*\(mosh-client\) \(-# \)//p' | cut -d ' ' -f1)
 
 if [[ -n $ssh_host ]]; then
   echo "#[fg=colour220]$ssh_host#[fg=default]"
+elif [[ -n $mosh_host ]]; then
+  echo "#[fg=colour220]$mosh_host#[fg=default]"
 else
 	hostname -s
 fi
