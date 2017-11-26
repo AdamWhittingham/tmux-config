70540fb113aad08a0571b9e926aeb50177561e06
Adam Whittingham <adam.whittingham@gmail.com>
2017-11-26 13:39:45 +0000

    Add Hostname change on SSH detection

---
+++ ssh-hostname.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
+ssh_host=$(ps -a | grep "$current_tty" | sed -n -e 's/^.*ssh //p')
+
+if [[ -n $ssh_host ]]; then
+  echo "#[fg=colour220]$ssh_host#[fg=default]"
+else
+	hostname -s
+fi
