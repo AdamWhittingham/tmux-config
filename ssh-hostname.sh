4671a65d699f6dde10f172e65e3ba10b3a7c9831
Adam Whittingham <adam.whittingham@gmail.com>
2017-11-26 13:54:18 +0000

    Remove need to grep

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
-ssh_host=$(ps -a | grep "$current_tty" | sed -n -e 's/^.*ssh //p')
+ssh_host=$(ps -t "$current_tty" | sed -n -e 's/^.*\(ssh\) //p')
 
 if [[ -n $ssh_host ]]; then
   echo "#[fg=colour220]$ssh_host#[fg=default]"
