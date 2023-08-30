e373e08c1b9172950148b1beed21f8b7687245c5
Adam Whittingham <adam.whittingham@gmail.com>
2023-08-30 14:03:37 +0100

    Nicer icon

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -4,7 +4,7 @@ current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
 ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
 
 if [[ -n $ssh_host ]]; then
-  echo "#[fg=colour178]#[fg=color234,bg=colour178,bright]󰇅 $ssh_host"
+  echo "#[fg=colour214]#[fg=color234,bg=colour214]󰇅 $ssh_host"
 else
-  echo "#[fg=colour61]#[fg=color234,bg=colour61]󰇅 $(hostname -s)"
+  echo "#[fg=colour61]#[fg=color234,bg=colour61] $(hostname -s)"
 fi
