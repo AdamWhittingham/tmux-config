04699258a9a946ebd1291ee8e6bc1c305e32701b
Adam Whittingham <adam.whittingham@gmail.com>
2023-08-30 18:51:13 +0100

    Switch colours

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -4,7 +4,7 @@ current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
 ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
 
 if [[ -n $ssh_host ]]; then
-  echo "#[fg=colour214]#[fg=color254,bg=colour214]󰇅 $ssh_host"
+  echo "#[fg=colour208]#[fg=color254,bg=colour208,bold]󰇅 $ssh_host#[nobold]"
 else
-  echo "#[fg=colour61]#[fg=color254,bg=colour61] $(hostname -s)"
+  echo "#[fg=colour25]#[fg=color254,bg=colour25] $(hostname -s)"
 fi
