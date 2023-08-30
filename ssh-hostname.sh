8c95fd486192b26e8645d75508328a6812fb15e2
Adam Whittingham <adam.whittingham@gmail.com>
2023-08-30 19:03:04 +0100

    Tidy up mode style

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -4,7 +4,7 @@ current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
 ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
 
 if [[ -n $ssh_host ]]; then
-  echo "#[fg=colour208]#[fg=color254,bg=colour208,bold]󰇅 $ssh_host#[nobold]"
+  echo "#[fg=colour208]#[fg=color254,bg=colour172,bold]󰇅 $ssh_host#[nobold]"
 else
   echo "#[fg=colour25]#[fg=color254,bg=colour25] $(hostname -s)"
 fi
