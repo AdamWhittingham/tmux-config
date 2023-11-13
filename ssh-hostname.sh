6caa33741200e1079966e5cc95dba9f5c2bd5053
Adam Whittingham <ad@cronofy.com>
2023-11-13 14:13:03 +0000

    Move session name

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -4,7 +4,7 @@ current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
 ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
 
 if [[ -n $ssh_host ]]; then
-  echo "#[fg=colour208]#[fg=color254,bg=colour172,bold]󰇅 $ssh_host#[nobold]"
+  echo "#[fg=color254,bg=colour172,bold]󰇅 $ssh_host#[nobold]#[fg=colour208]"
 else
-  echo "#[fg=colour25]#[fg=color254,bg=colour25] $(hostname -s)"
+  echo "#[fg=color254,bg=colour25]  $(hostname -s)#[fg=colour25,bg=default]"
 fi
