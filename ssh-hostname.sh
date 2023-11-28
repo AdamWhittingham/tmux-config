ff240da4a8f0a7a1bdfdbe932232d3d47a6822c6
Adam Whittingham <ad@cronofy.com>
2023-11-28 09:45:23 +0000

    Correct ssh info server format

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -4,7 +4,7 @@ current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
 ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
 
 if [[ -n $ssh_host ]]; then
-  echo "#[fg=color254,bg=colour172,bold]󰇅 $ssh_host#[nobold]#[fg=colour208]"
+  echo "#[fg=color254,bg=colour172,bold]󰇅 $ssh_host#[nobold]#[fg=colour172,bg=default]"
 else
   echo "#[fg=color254,bg=colour25]  $(hostname -s)#[fg=colour25,bg=default]"
 fi
