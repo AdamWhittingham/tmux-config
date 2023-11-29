dc520d8926666cb43b5510d00ba76c6f7c8502d3
Adam Whittingham <adam.whittingham@gmail.com>
2023-11-29 21:06:24 +0000

    Tweak colours

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -4,7 +4,7 @@ current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
 ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
 
 if [[ -n $ssh_host ]]; then
-  echo "#[fg=color254,bg=colour172,bold]󰇅 $ssh_host#[nobold]#[fg=colour172,bg=default]"
+  echo "#[fg=color254,bg=colour162,bold]󰇅 $ssh_host#[nobold]#[fg=colour162,bg=default]"
 else
-  echo "#[fg=color254,bg=colour25]  $(hostname -s)#[fg=colour25,bg=default]"
+  echo "#[fg=color254,bg=colour23]  $(hostname -s)#[fg=colour23,bg=default]"
 fi
