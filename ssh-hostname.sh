3e9bb2a7beda1b38f1d0c25c41bf5508d8107dcf
Adam Whittingham <adam.whittingham@gmail.com>
2024-01-09 21:42:58 +0000

    Tidy up bar element color vars

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -6,5 +6,5 @@ ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
 if [[ -n $ssh_host ]]; then
   echo "#[fg=color254,bg=colour162,bold]󰇅 $ssh_host#[nobold]#[fg=colour162,bg=default]"
 else
-  echo "#[fg=color254,bg=colour23]  $(hostname -s)#[fg=colour23,bg=default]"
+  echo "#[fg=#{@BAR_FG},bg=#{@BAR_BG}]  $(hostname -s)#[fg=#{@BAR_BG},bg=default]"
 fi
