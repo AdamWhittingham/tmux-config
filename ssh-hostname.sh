0425c76d194d988835c0df40ae13527ac3d4c0ea
Adam Whittingham <adam.whittingham@gmail.com>
2024-01-12 13:59:46 +0000

    Tidy up ssh host ping

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+fg=$2
+bg=$3
 current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
 ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
 
 if [[ -n $ssh_host ]]; then
-  echo "#[fg=color254,bg=colour162,bold]󰇅 $ssh_host#[nobold]#[fg=colour162,bg=default]"
+  echo "#[fg=$fg,bg=$bg]󰇅 $ssh_host#[fg=$bg,bg=default]"
 else
   echo "#[fg=#{@BAR_FG},bg=#{@BAR_BG}]  $(hostname -s)#[fg=#{@BAR_BG},bg=default]"
 fi
