6cc4b7f0d3cda5457eadd9e5590705cca7aad29b
Adam Whittingham <adam.whittingham@gmail.com>
2024-01-14 21:04:26 +0000

    Make SSH text bold

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -6,7 +6,7 @@ current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
 ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
 
 if [[ -n $ssh_host ]]; then
-  echo "#[fg=$fg,bg=$bg]󰇅 $ssh_host#[fg=$bg,bg=default]"
+  echo "#[fg=$fg,bg=$bg,bold]󰇅 $ssh_host#[fg=$bg,bg=default,nobold]"
 else
   echo "#[fg=#{@BAR_FG},bg=#{@BAR_BG}]  $(hostname -s)#[fg=#{@BAR_BG},bg=default]"
 fi
