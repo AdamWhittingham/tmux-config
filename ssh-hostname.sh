6535bd4de927be305d07aeefbe88ff1723de5af4
Adam Whittingham <ad@cronofy.com>
2025-03-12 07:26:14 +0000

    Improve ssh hostname

--- ssh-hostname.sh
+++ ssh-hostname.sh
@@ -3,10 +3,10 @@
 fg=$2
 bg=$3
 current_tty="$(echo $1 | rev | cut -d'/' -f1 | rev)"
-ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" | cut -f2 -d' ')
+ssh_host=$(ps -t "$current_tty" -o args | grep "ssh" |  grep -o "\w*@\w*")
 
 if [[ -n $ssh_host ]]; then
-  echo "#[fg=$fg,bg=$bg,bold]󰇅 $ssh_host#[fg=$bg,bg=default,nobold]"
+  echo "#[fg=$bg,bg=default,nobold]#[fg=$fg,bg=$bg,bold]󰇅 $ssh_host#[fg=$bg,bg=default,nobold]"
 else
-  echo "#[fg=#{@BAR_FG},bg=#{@BAR_BG}]  $(hostname -s)#[fg=#{@BAR_BG},bg=default]"
+  echo "#[fg=#{@BAR_BG},bg=default]#[fg=#{@BAR_FG},bg=#{@BAR_BG}] $(hostname -s)#[fg=#{@BAR_BG},bg=default]"
 fi
